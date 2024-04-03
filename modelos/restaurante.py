588014643fee06894e6f89724ee02356037cfab0
taiane94sb <taiane19sb94@gmail.com>
2024-04-02 22:21:14 -0300

    métodos de classes

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -10,14 +10,18 @@ class Restaurante:
     def __str__(self):
         return f'{self._nome} {self._categoria}'
     
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
         print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
-        for restaurante in Restaurante.restaurantes:
+        for restaurante in cls.restaurantes:
             print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante._ativo}')
             
     @property
     def ativo(self):
         return '⌧' if self._ativo else '☐'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
@@ -25,7 +29,13 @@ restaurante_pizza = Restaurante('pizza express', 'Italiana')
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(restaurante_praca)
-print(restaurante_pizza)
+print(restaurante_praca.ativo)
+print(restaurante_pizza.ativo)
+Restaurante.listar_restaurantes()
 
+restaurante_praca.alternar_estado()
+restaurante_pizza.alternar_estado()
+
+print(restaurante_praca.ativo)
+print(restaurante_pizza.ativo)
 Restaurante.listar_restaurantes()

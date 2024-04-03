42e514982416e65f567e9f1786e7883937999ba7
taiane94sb <taiane19sb94@gmail.com>
2024-04-02 22:05:21 -0300

    criando meus métodos

--- restaurante.py
+++ restaurante.py
@@ -1,11 +1,18 @@
 class Restaurante:
+    restaurantes = []
+    
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
-        
+        Restaurante.restaurantes.append(self)
+    
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
@@ -15,3 +22,6 @@ restaurantes = [restaurante_praca, restaurante_pizza]
 
 print(restaurante_praca)
 print(restaurante_pizza)
+
+print('Listando restaurantes: ')
+Restaurante.listar_restaurantes()

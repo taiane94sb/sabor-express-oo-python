a1d106fe04eef09972b9adaca33582cf3aced060
taiane94sb <taiane19sb94@gmail.com>
2024-04-02 22:02:08 -0300

    métodos especiais

--- restaurante.py
+++ restaurante.py
@@ -3,17 +3,15 @@ class Restaurante:
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
 
 
-# restaurante_praca = Restaurante()
-# restaurante_praca.nome = 'Praça'
-# restaurante_praca.categoria = 'Gourmet'
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(dir(restaurante_praca))
-print(vars(restaurante_praca))
-print(dir(restaurante_pizza))
-print(vars(restaurante_pizza))
+print(restaurante_praca)
+print(restaurante_pizza)
